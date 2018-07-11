32392502d2332373aa7d8808edc1c34719e42d95
Patrick Chan <patrickchan1999@gmail.com>
2018-07-11 11:28:23 -0400

    Remove searcher.rs, add search.rs and searcher/mod.rs - search.rs handles the thread spawning - searcher directory would contain implementations of searchers to search different bits of information

---
+++ src/search.rs
@@ -0,0 +1,64 @@
+use sciter;
+
+use std::{thread, time};
+use std::sync::mpsc;
+
+use searcher::Searcher;
+
+pub fn spawn_search_thread(receiver: mpsc::Receiver<String>, root: sciter::Element) {
+    thread::spawn(move || {
+        let (mut _search_sender, mut search_receiver): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel();
+        let mut still_receiving = false;
+        loop {
+            match receiver.try_recv() {
+                Ok(search_term) => {
+                    let (search_sender_clone, search_receiver_clone) = mpsc::channel();
+                    _search_sender = search_sender_clone;
+                    search_receiver = search_receiver_clone;
+                    let _ = root.call_function("search.clearQueueAndSearchResult", &make_args!());
+                    if search_term != "" {
+                        spawn_search_worker(search_term, _search_sender);
+                        still_receiving = true;
+                    }
+                }
+                Err(mpsc::TryRecvError::Disconnected) => {
+                    println!("Terminated!");
+                    break;
+                }
+                Err(mpsc::TryRecvError::Empty) => {}
+            }
+            if still_receiving {
+                still_receiving = send_candidates_to_queue(&search_receiver, &root);
+            }
+        }
+    });
+}
+
+fn spawn_search_worker(search_term: String, sender: mpsc::Sender<String>) {
+    thread::spawn(move || {
+        let searcher = Searcher::new(search_term);
+        let result = searcher.search();
+        for candidate in &result {
+            thread::sleep(time::Duration::from_millis(100));
+            let send_result = sender.send(candidate.to_string());
+            if send_result.is_err() {
+                break;
+            }
+        }
+    });
+}
+
+fn send_candidates_to_queue(receiver: &mpsc::Receiver<String>, root: &sciter::Element) -> bool {
+    let mut still_receiving = true;
+    match receiver.try_recv() {
+        Ok(candidate) => {
+            let _ = root.call_function("search.addToQueue", &make_args!(candidate));
+        }
+        Err(mpsc::TryRecvError::Disconnected) => {
+            println!("Terminated!");
+            still_receiving = false;
+        }
+        Err(mpsc::TryRecvError::Empty) => {}
+    }
+    still_receiving
+}
