76afec2e12f72fe352c970fc14a62473e8135bf3
Patrick Chan <patrickchan1999@gmail.com>
2018-07-28 16:39:05 -0400

    Update load handler to work with local file paths

--- src/load_handler.rs
+++ src/load_handler.rs
@@ -1,6 +1,8 @@
 extern crate sciter;
 
 use sciter::host;
+use std::fs::File;
+use std::io::Read;
 
 pub struct LoadHandler {
     archive: host::Archive,
@@ -25,7 +27,15 @@ impl sciter::HostHandler for LoadHandler {
             if let Some(data) = self.archive.get(&uri) {
                 self.data_ready(pnm.hwnd, &uri, data, None);
             } else {
-                eprintln!("[handler] error: can't load {:?}", uri);
+                let filename = uri["this://app".to_string().len()..].to_owned();
+                match File::open(filename.clone()) {
+                    Ok(mut file) => {
+                        let mut content = Vec::new();
+                        file.read_to_end(&mut content).expect("Something went wrong while reading file");
+                        self.data_ready(pnm.hwnd, &uri, &content, None);
+                    }
+                    Err(err) => { eprintln!("[handler] error: can't load {} because of {}", filename, err); }
+                }
             }
         }
         return Some(host::LOAD_RESULT::LOAD_DEFAULT);
